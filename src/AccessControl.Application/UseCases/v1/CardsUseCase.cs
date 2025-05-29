e469fbad929c2519df136f6a9bd2884f4bab479d
Bruno Trindade Bragança <bbmendes@gmail.com>
2025-05-29 15:38:53 -0300

    Added serilog

--- src/AccessControl.Application/UseCases/v1/CardsUseCase.cs
+++ src/AccessControl.Application/UseCases/v1/CardsUseCase.cs
@@ -1,30 +1,37 @@
 ﻿using AccessControl.Domain.Interfaces.v1.Repository;
 using AccessControl.Domain.Interfaces.v1.UseCases;
 using AccessControl.Domain.Models;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.Extensions.Logging;
 
 namespace AccessControl.Application.UseCases.v1
 {
-    public class CardsUseCase(ICardsRepository cardsRepository) : ICardsUseCase
+    public class CardsUseCase(ICardsRepository cardsRepository, ILogger<CardsUseCase> logger) : ICardsUseCase
     {
         private readonly ICardsRepository _cardsRepository = cardsRepository ?? throw new ArgumentNullException(nameof(cardsRepository));
+        private readonly ILogger<CardsUseCase> _logger = logger ?? throw new ArgumentNullException(nameof(logger));
 
         public async Task<string> CreateCard(int cardNumber, string firstName, string lastName)
         {
+            _logger.LogInformation(nameof(CreateCard));
             return await _cardsRepository.AddCard(cardNumber, firstName, lastName);
         }
 
         public async Task<string> GrantAccess(int cardNumber, int doorNumber)
         {
+            _logger.LogInformation(nameof(GrantAccess));
             return await _cardsRepository.GrantAccess(cardNumber, doorNumber);
         }
 
         public async Task<string> CancelPermission(int cardNumber, int doorNumber)
         {
+            _logger.LogInformation(nameof(CancelPermission));
             return await _cardsRepository.CancelPermission(cardNumber, doorNumber);
         }
 
         public async Task<IQueryable<Card>> ListCards()
         {
+            _logger.LogInformation(nameof(ListCards));
             return await _cardsRepository.ListCards();
         }
     }
--- src/AccessControl/Program.cs
+++ src/AccessControl/Program.cs
@@ -7,54 +7,70 @@ using AccessControl.Infrastructure;
 using AccessControl.Infrastructure.Repository.v1;
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using Serilog;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Adding context
-builder.Services.AddDbContext<DBContext>();
-
-// Adding exception handling
-builder.Services.AddExceptionHandler<GeneralExceptionHandler>();
-builder.Services.AddProblemDetails();
-
-builder.Services
-    .AddControllers()
-    .AddJsonOptions(options => options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
-
-
-// Adding swagger
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(config => 
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateLogger();
+try
 {
-    config.SchemaFilter<EnumSchemaFilter>();
-    config.SwaggerDoc("v1", new() { Title = "Access Control API", Version = "v1" });
-});
+    var builder = WebApplication.CreateBuilder(args);
+    builder.Services.AddSerilog();
 
-// Adding Use Cases
-builder.Services.AddScoped<ICardsUseCase, CardsUseCase>();
-builder.Services.AddScoped<IDoorsUseCase, DoorsUseCase>();
+    // Adding context
+    builder.Services.AddDbContext<DBContext>();
 
-// Adding Repositories
-builder.Services.AddScoped<ICardsRepository, CardsRepository>();
-builder.Services.AddScoped<IDoorsRepository, DoorsRepository>();
+    // Adding exception handling
+    builder.Services.AddExceptionHandler<GeneralExceptionHandler>();
+    builder.Services.AddProblemDetails();
 
-var app = builder.Build();
+    builder.Services
+        .AddControllers()
+        .AddJsonOptions(options => options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
 
-app.UseExceptionHandler();
 
-// Enabling swagger only for development
-if (app.Environment.IsDevelopment())
-{
-    app.UseDeveloperExceptionPage();
-    app.UseSwagger();
-    app.UseSwaggerUI(options =>
+    // Adding swagger
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen(config => 
     {
-        options.SwaggerEndpoint("swagger/v1/swagger.json", "v1");
+        config.SchemaFilter<EnumSchemaFilter>();
+        config.SwaggerDoc("v1", new() { Title = "Access Control API", Version = "v1" });
     });
+
+    // Adding Use Cases
+    builder.Services.AddScoped<ICardsUseCase, CardsUseCase>();
+    builder.Services.AddScoped<IDoorsUseCase, DoorsUseCase>();
+
+    // Adding Repositories
+    builder.Services.AddScoped<ICardsRepository, CardsRepository>();
+    builder.Services.AddScoped<IDoorsRepository, DoorsRepository>();
+
+    var app = builder.Build();
+
+    app.UseExceptionHandler();
+
+    // Enabling swagger only for development
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseDeveloperExceptionPage();
+        app.UseSwagger();
+        app.UseSwaggerUI(options =>
+        {
+            options.SwaggerEndpoint("swagger/v1/swagger.json", "v1");
+        });
+    }
+
+    app.UseRouting();
+
+    app.MapControllers();
+
+    app.Run();
 }
-
-app.UseRouting();
-
-app.MapControllers();
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
